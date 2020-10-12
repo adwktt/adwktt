39ae04219c8c867df35b0c82343f3c20e76aeacb
adwktt <63175152+adwktt@users.noreply.github.com>
2020-10-12 23:59:02 +0800

    Update rrspresponse.js

--- rrspresponse.js
+++ rrspresponse.js
@@ -5,9 +5,6 @@ obj.data.user.vipMedal = {"name":"大魔王","endTime":"2099-12-12 12:12:12","im
 obj.data.user.achievementCount = 10000;
 obj.data.user.level = 30;
  };
-    if($request.url.indexOf('/v3plus/medal/getAllMedalDetailWithPiece') != -1){
-obj.data.medalList.medal.endTime= "2099-12-12 12:12:12";
- };
     if($request.url.indexOf('v3plus/user/detail') != -1){
 obj.data.user.medalList = [{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":6},{"id":7},{"id":8},{"id":9},{"id":10},{"id":11},{"id":12},{"id":13},{"id":14},{"id":15},{"id":17},{"id":18},{"id":19},{"id":20},{"id":21},{"id":22},{"id":23},{"id":33}];
 obj.data.user.achievementCount = 10000;
