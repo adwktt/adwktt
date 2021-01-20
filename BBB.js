b224c26be32b17abf710985370c1c824e145cfa4
adwktt <63175152+adwktt@users.noreply.github.com>
2021-01-20 16:48:35 +0800

    Update BBB.js

--- BBB.js
+++ BBB.js
@@ -65,6 +65,7 @@ $.msg($.name,"開始🎉🎉🎉")
       await getNewsId()
       await getQuestionId()
       await checkHomeJin()
+      await guaList()
       await showmsg()
 
 })()
@@ -217,6 +218,102 @@ return new Promise((resolve, reject) => {
    })
   } 
 
+
+function guaList() {
+return new Promise((resolve, reject) => {
+  let timestamp=new Date().getTime();
+  let gualist ={
+    url: `https://bububao.duoshoutuan.com/gua/gualist?`,
+    headers: JSON.parse(CookieVal),
+}
+   $.post(gualist,async(error, response, data) =>{
+$.log('\n🔔開始查詢刮刮卡ID\n')
+     const guaid = JSON.parse(data)
+      if(guaid.ka > 0){
+      for (guaId of guaid.list)
+      if(guaId.is_ad == 0)
+$.log('\n🔔查詢刮刮卡ID成功,5s後開始刮卡\n')
+      guaID = guaId.id
+$.log('\nID: '+guaID+'\n')
+          await $.wait(5000)
+          await guaDet()
+         }else{
+$.log('\n⚠️刮刮卡已用完,請明天再刮吧！\n')
+        }
+
+          resolve()
+    })
+   })
+  } 
+
+function guaDet() {
+return new Promise((resolve, reject) => {
+  let timestamp=new Date().getTime();
+  let guadet ={
+    url: `https://bububao.duoshoutuan.com/gua/guadet?`,
+    headers: JSON.parse(CookieVal),
+    body: `gid=${guaID}&`
+}
+   $.post(guadet,async(error, response, data) =>{
+$.log('\n🔔開始查詢刮卡簽名\n')
+     const guasign= JSON.parse(data)
+      if(response.statusCode == 200) {
+$.log('\n🔔查詢刮卡簽名成功\n')
+      SIGN = guasign.sign
+      GLID = guasign.glid
+$.log('\nsign: '+SIGN+'\n')
+$.log('\nglid: '+GLID+'\n')
+          await guaPost()
+         }
+          resolve()
+    })
+   })
+  } 
+
+function guaPost() {
+return new Promise((resolve, reject) => {
+  let timestamp=new Date().getTime();
+  let guapost ={
+    url: `https://bububao.duoshoutuan.com/gua/guapost?`,
+    headers: JSON.parse(CookieVal),
+    body: `sign=${SIGN}&gid=${guaID}&glid=${GLID}&`
+}
+   $.post(guapost,async(error, response, data) =>{
+$.log('\n🔔開始刮卡\n')
+     const guaka= JSON.parse(data)
+      if(typeof guaka.jf === 'number') {
+      guaStr = guaka.nonce_str
+          $.log('\n🎉刮卡成功\n恭喜您刮出'+guaka.tp+'張相同圖案\n金幣+ '+guaka.jf+'\n等待45s後開始翻倍刮卡獎勵')
+          await $.wait(45000)
+          await guaDouble()
+         }
+          resolve()
+    })
+   })
+  } 
+
+
+function guaDouble() {
+return new Promise((resolve, reject) => {
+  let timestamp=new Date().getTime();
+  let guadouble ={
+    url: `https://bububao.duoshoutuan.com/you/callback`,
+    headers: JSON.parse(CookieVal),
+    body: `nonce_str=${guaStr}&tid=6&pos=1&`,
+}
+   $.post(guadouble,async(error, response, data) =>{
+     const guaka2 = JSON.parse(data)
+$.log('\n🔔開始領取每日觀看獎勵\n')
+      if(guaka2.code == 1) {
+          $.log('\n🎉刮卡翻倍成功,等待2s後查詢下一張刮刮卡ID\n')
+          await $.wait(2000)
+           }else{
+          $.log('\n⚠️刮卡翻倍失敗:'+guaka2.msg+'\n')
+           }
+          resolve()
+    })
+   })
+  } 
 
 
 
